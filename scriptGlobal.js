d87acd93431908e640314c19c3ce220779502162
Rafaella Cristina <rsacramento@osas.com.br>
2025-06-07 17:50:44 -0300

    alterando local onde é chamada as alteracoes do calendario

--- scriptGlobal.js
+++ scriptGlobal.js
@@ -14,6 +14,7 @@ async function executarScriptsEspecificos(nome) {
                 preencherAlunosVinculados();
             }
             getAnotacoesBlocoNotas();
+            preencherCalendario()
             break;
         case "InserirMateriais":
             preencherNomesMaterias();
